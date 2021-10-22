360e00bd546d64cc4212df1f9c257362c5183426
Roy Tang <hkmute@gmail.com>
2021-10-22 15:14:28 +0800

    improve TodayScreen loading screen

--- app-client/src/components/Choice.tsx
+++ app-client/src/components/Choice.tsx
@@ -1,97 +1,30 @@
-import { API_BASE_URL } from "@env";
-import { useFocusEffect } from "@react-navigation/native";
-import React, { useCallback, useContext, useState } from "react";
+import React from "react";
 import { View, Button, Text } from "react-native";
-import { User } from "../../AppContext";
 import styles from "../styles/styles";
+import { Option, Vote } from "../screens/TodayScreen";
 
-interface Option {
-  id: number;
-  date: string;
-  name: string;
-}
-
-export default function Choice(props: { code: string }) {
-  const [options, setOptions] = useState<Option[]>([]);
-  const [vote, setVote] = useState();
-  const user = useContext(User);
-
-  useFocusEffect(
-    useCallback(() => {
-      let isActive = true;
-      fetchTodayVote().then((vote) => {
-        if (vote?.data.length) {
-          if (isActive) {
-            setVote(vote.data[0].name);
-          }
-          return;
-        }
-        fetchTodayOptions().then((result) => {
-          if (isActive) {
-            setOptions(result.data);
-          }
-        });
-      });
-      return () => {
-        isActive = false;
-      };
-    }, [])
-  );
-
-  const fetchTodayOptions = async () => {
-    try {
-      const res = await fetch(`${API_BASE_URL}/today-options/${props.code}`);
-      return await res.json();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const fetchTodayVote = async () => {
-    try {
-      const res = await fetch(
-        `${API_BASE_URL}/votes/${props.code}?user=${user}`
-      );
-      return await res.json();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handlePress = (id: number) => async () => {
-    try {
-      const res = await fetch(`${API_BASE_URL}/votes/${props.code}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          user,
-          optionId: id,
-        }),
-      });
-      if (res.ok) {
-        const vote = await fetchTodayVote();
-        if (vote.data.length) {
-          setVote(vote.data[0].name);
-        }
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
+export default function Choice(props: {
+  code: string;
+  options: Option[];
+  vote: Vote[];
+  handlePress: (id: number) => () => Promise<void>;
+}) {
   return (
     <View style={{ ...styles.container, justifyContent: "center", margin: 16 }}>
-      {vote ? (
+      {props.vote ? (
         <>
-          <Text style={{ textAlign: "center" }}>你今天的選擇是 {vote}</Text>
+          <Text style={{ textAlign: "center" }}>
+            你今天的選擇是 {props.vote}
+          </Text>
           <Text style={{ textAlign: "center" }}>結果會在中午12時抽出</Text>
         </>
-      ) : options.length ? (
-        options.map((option) => (
+      ) : props.options.length ? (
+        props.options.map((option) => (
           <View key={option.id} style={{ paddingVertical: 16 }}>
-            <Button title={option.name} onPress={handlePress(option.id)} />
+            <Button
+              title={option.name}
+              onPress={props.handlePress(option.id)}
+            />
           </View>
         ))
       ) : (
--- app-client/src/screens/TodayScreen.tsx
+++ app-client/src/screens/TodayScreen.tsx
@@ -1,25 +1,54 @@
 import { API_BASE_URL } from "@env";
 import { useFocusEffect } from "@react-navigation/native";
 import React, { useCallback, useContext, useState } from "react";
-import { ActivityIndicator, View } from "react-native";
-import { RoomCode } from "../../AppContext";
+import { ActivityIndicator, View, Text } from "react-native";
+import { RoomCode, User } from "../../AppContext";
 import Choice from "../components/Choice";
 import Result from "../components/Result";
 import styles from "../styles/styles";
 import * as Sentry from "sentry-expo";
 
+export interface Option {
+  id: number;
+  date: string;
+  name: string;
+}
+
+export interface Vote {
+  id: number;
+  date: string;
+  name: string;
+  user: string;
+  optionId: number;
+}
+
 export default function TodayScreen() {
-  const [loading, setLoading] = useState(true);
   const [result, setResult] = useState("");
+  const [options, setOptions] = useState<Option[]>([]);
+  const [vote, setVote] = useState<Vote[]>([]);
+  const loading = !result && !options.length && !vote.length;
   const code = useContext(RoomCode);
+  const user = useContext(User);
+
   useFocusEffect(
     useCallback(() => {
       let isActive = true;
       fetchTodayResult().then((result) => {
         if (isActive) {
-          setResult(result?.data?.name ?? null);
+          if (result) {
+            return setResult(result?.data?.name ?? null);
+          }
+          fetchTodayVote().then((vote) => {
+            if (isActive) {
+              if (vote?.data.length) {
+                return setVote(vote.data[0].name);
+              }
+              fetchTodayOptions().then((result) => {
+                return setOptions(result.data);
+              });
+            }
+          });
         }
-        return;
       });
       return () => {
         isActive = false;
@@ -29,32 +58,79 @@ export default function TodayScreen() {
 
   const fetchTodayResult = async () => {
     try {
-      setLoading(true);
       const res = await fetch(`${API_BASE_URL}/history/${code}/today`);
       return await res.json();
     } catch (err) {
       console.log(err);
       Sentry.Native.captureException(err);
-    } finally {
-      setLoading(false);
+    }
+  };
+
+  const fetchTodayOptions = async () => {
+    try {
+      const res = await fetch(`${API_BASE_URL}/today-options/${code}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchTodayVote = async () => {
+    try {
+      const res = await fetch(`${API_BASE_URL}/votes/${code}?user=${user}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handlePress = (id: number) => async () => {
+    try {
+      const res = await fetch(`${API_BASE_URL}/votes/${code}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user,
+          optionId: id,
+        }),
+      });
+      if (res.ok) {
+        const vote = await fetchTodayVote();
+        if (vote.data.length) {
+          setVote(vote.data[0].name);
+        }
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
   return (
     <View style={styles.container}>
-      {loading && (
+      {loading ? (
         <View
           style={{
             position: "absolute",
             height: "100%",
             width: "100%",
             justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <ActivityIndicator />
+          <ActivityIndicator size="large" color="#999" />
         </View>
+      ) : result ? (
+        <Result result={result} />
+      ) : (
+        <Choice
+          code={code}
+          options={options}
+          vote={vote}
+          handlePress={handlePress}
+        />
       )}
-      {result ? <Result result={result} /> : <Choice code={code} />}
     </View>
   );
 }
